4be5cdb6eac01e1bc190e17027f8fcd2d34b44cd
william <wedderburn95@yahoo.com>
2021-06-01 23:13:30 -0400

    commit tree 9d7ad582c152e6c558262ea3f6b701a7527cf1e6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+  
+// this is what will be passed in:
+// findMatching(drivers, 'Bobby');
+
+function findMatching(driverArray, name) {
+    // for our filter condition we just need to see if the driver's name is the name
+    // we're being very careful with what we name things here:
+    return driverArray.filter(driverName => driverName.toLowerCase() === name.toLowerCase());
+  };
+  
+  function fuzzyMatch(driverArray, name) {
+    // we only want to match the first letter so we can access it with the index of the string:
+    return driverArray.filter(person => person[0].toLowerCase() === name[0].toLowerCase());
+  };
+  
+  function matchName(driverArray, name) {
+    // person is an object so we want to specify that we need their name:
+    return driverArray.filter(personObject => personObject.name === name);
+  };
+
+  
